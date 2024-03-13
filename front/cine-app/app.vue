248e7c96f6da57004ac83b074e7531690bd2a8b9
julsluks <a22betvilver@inspedralbes.cat>
2024-03-13 21:10:52 +0100

    nuxt try

--- front/cine-app/app.vue
+++ front/cine-app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>CinemaVilla</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
   </div>
 </template>
---
+++ front/cine-app/pages/landing.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Home Page, cinemaVilla</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
