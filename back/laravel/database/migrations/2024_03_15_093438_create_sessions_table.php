10db09422e3c7dcac108ec2ca642d90ea7a03efd
julsluks <a22betvilver@inspedralbes.cat>
2024-03-15 10:49:52 +0100

    create sessions

---
+++ back/laravel/database/migrations/2024_03_15_093438_create_sessions_table.php
@@ -0,0 +1,106 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->date('day');
+            $table->time('hour');
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->integer('total_tickets');
+            $table->integer('tickets_sold');
+            $table->timestamps();
+        });
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-15', 
+            'hour' => '18:00:00', 
+            'movie_id' => '1', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-16', 
+            'hour' => '18:00:00', 
+            'movie_id' => '6', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-17', 
+            'hour' => '18:00:00', 
+            'movie_id' => '8', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-18', 
+            'hour' => '18:00:00', 
+            'movie_id' => '3', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-19', 
+            'hour' => '18:00:00', 
+            'movie_id' => '12', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-20', 
+            'hour' => '18:00:00', 
+            'movie_id' => '17', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-21', 
+            'hour' => '18:00:00', 
+            'movie_id' => '14', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-22', 
+            'hour' => '18:00:00', 
+            'movie_id' => '0', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+
+        DB::table('sessions')->insert([
+            'day' => '2024-04-23', 
+            'hour' => '18:00:00', 
+            'movie_id' => '16', 
+            'total_tickets' => 120, 
+            'tickets_sold' => 0, 
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\SessionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use App\Http\Controllers\MoviesController;
 */
 
 Route::get('/movies', [MoviesController::class, 'index']);
+Route::post('/addMovie', [MoviesController::class,'create']);
+
+Route::get('/sessions', [SessionsController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
