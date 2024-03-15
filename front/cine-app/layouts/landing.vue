4662f831af2aab78ea9e2607b5c186dce2471bb1
julsluks <a22betvilver@inspedralbes.cat>
2024-03-15 20:09:33 +0100

    cartelera lista

--- front/cine-app/layouts/landing.vue
+++ front/cine-app/layouts/landing.vue
@@ -1,12 +1,13 @@
 <template>
   <HeaderLanding />
-  <div class="flex flex-wrap justify-center">
+  <div class="flex flex-wrap justify-center bg-gray-100">
     <div v-for="movie in movies" :key="movie.id" class="m-4 cursor-pointer" @click="verDetalle(movie.id)">
-      <img :src="movie.image" :alt="movie.title" class="mb-2">
-      <p class="text-center">{{ movie.title }}</p>
+      <div class="movie-card p-2 rounded-lg shadow-lg bg-white hover:shadow-2xl transition duration-300 ease-in-out">
+        <img :src="movie.image" :alt="movie.title" class="mb-2 rounded-lg">
+        <p class="p-2 text-center text-3xl font-semibold">{{ movie.title }}</p>
+      </div>
     </div>
   </div>
-    
 </template>
 
 <script>
