2a2e8960cdad84b798876e53e84ff66f831e9ac9
julsluks <a22betvilver@inspedralbes.cat>
2024-03-15 18:28:51 +0100

    solucionar error nuxt

--- front/cine-app/app.vue
+++ front/cine-app/app.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
     <Navbar />
-    <!-- <h1>CinemaVilla</h1> -->
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav> -->
-    <router-view />
+    <NuxtPage :name="index" />    
     <Footer />
-    
   </div>
 </template>
 
 <script setup>
-  import Navbar from './components/navbar.vue';
-  import Footer from './components/footer.vue';
 </script>
--- front/cine-app/pages/index.vue
+++ front/cine-app/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <Landing />
+        <NuxtLayout name="landing" />
     </div>
 </template>
 
 <script setup>
-    import Landing from '~/layouts/landing.vue';
+
 </script>
 
 <style scoped>
