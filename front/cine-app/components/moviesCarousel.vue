641c2e66d0a9f26701dc80ed768312a7f0748eb5
julsluks <a22betvilver@inspedralbes.cat>
2024-03-20 22:05:23 +0100

    carousel hecho

--- front/cine-app/components/moviesCarousel.vue
+++ front/cine-app/components/moviesCarousel.vue
@@ -1,13 +1,40 @@
 <template>
-
+    <div class="overflow-x-auto whitespace-nowrap custom-scrollbar bg-blue-950">
+        <div class="flex space-x-4 bg-blue-950">
+            <div v-for="(movie, index) in movies" :key="index" class="w-48 flex-shrink-0 bg-blue-950">
+                <nuxt-link :to="{ name: 'movies-id', params: { id: movie.id_movie } }">
+                    <div class="w-full bg-blue-950 rounded-lg shadow-md p-2">
+                        <img :src="movie.image" class="w-full h-auto rounded-lg bg-blue-950"
+                            :alt="`Movie poster for ${movie.title}`">
+                    </div>
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
         movies: Object
-    },
-    methods: {
     }
 };
 </script>
+
+<style scoped>
+.custom-scrollbar::-webkit-scrollbar {
+    width: 4px;
+    /* Ajusta este valor para cambiar el ancho del scrollbar */
+}
+
+.custom-scrollbar::-webkit-scrollbar-track {
+    background: #1a202c;
+    /* Cambia este color según lo necesites */
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb {
+    background-color: #4a5568;
+    /* Cambia este color según lo necesites */
+    border-radius: 3px;
+}
+</style>
